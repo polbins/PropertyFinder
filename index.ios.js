b45c856ff16cf0bdc43b7a2fece3e69858046206
Vince Intal <vince@ninjavan.co>
2017-01-31 11:12:26 +0800

    add SearchPage

--- index.ios.js
+++ index.ios.js
@@ -4,8 +4,11 @@ import React from 'react';
 import {
     AppRegistry,
     StyleSheet,
+    NavigatorIOS,
     Text
 } from 'react-native';
+
+import SearchPage from './SearchPage';
 
 const styles = StyleSheet.create({
     text: {
@@ -13,14 +16,22 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         fontSize: 30,
         margin: 80
+    },
+    container: {
+        flex: 1
     }
 });
 
 class PropertyFinderApp extends React.Component {
     render() {
         return (
-            <Text style={styles.text}>Hello World!</Text>
-        )
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: 'Property Finder',
+                    component: SearchPage,
+                }}/>
+            )
     }
 }
 
